78374d98d239e7845d33cf90e6fe974eb9b4eeac
Vatsal <vatsalpatel1073@gmail.com>
2021-02-11 10:11:21 +0530

    Finished Banner.. hooking up API now

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './Components/HomeScreen';
-
+import React from "react";
+import "./App.css";
+import HomeScreen from "./Components/HomeScreen";
 
 function App() {
   return (
-    <div className="app">
-     <HomeScreen/>
-    
-
-
-
-
-
-
+    <div className='app'>
+      <HomeScreen />
     </div>
   );
 }
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import '../Stylesheets/Banner.css'
+import React from "react";
+import "../Stylesheets/Banner.css";
 
 const Banner = () => {
-    return (
-        <header className="banner"  
-        style = {{ 
-        backgroundSize : 'cover' ,
-        backgroundImage : `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuTL9nyf11D_hcLE9dHkvLzwj6U684U0j_eg&usqp=CAU)`
-    }}></header>
-    ) 
-}
+  const truncate = (string, n) => {
+    return string?.length > n ? string.substring(0, n - 1) + "..." : string;
+  };
 
-export default Banner
+  return (
+    <header
+      className='banner'
+      style={{
+        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center", // makes sure the image stays in center
+      }}>
+      <div className='banner__contents'>
+        <h1 className='banner__title'>Movie Name</h1>
+        <div className='banner__buttons'>
+          <button className='banner__button'>Play</button>
+          <button className='banner__button'>Add to My List</button>
+        </div>
+        <h1 className='banner__description'>
+          {truncate("test test test ", 150)}
+        </h1>
+      </div>
+      <div className='banner--fadebottom' />
+    </header>
+  );
+};
+
+export default Banner;
